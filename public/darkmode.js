5633822159156015034cd89c4e4f403e5325f8e5
Ignesh <igneshrai.com>
2025-07-10 12:58:46 +0530

    🌙 Add dark mode toggle feature and structure

---
+++ public/darkmode.js
@@ -0,0 +1,11 @@
+
+function toggleDarkMode() {
+  document.body.classList.toggle("dark-mode");
+
+  const icon = document.querySelector(".slider .icon");
+  if (document.body.classList.contains("dark-mode")) {
+    icon.textContent = "🌙";
+  } else {
+    icon.textContent = "🌞";
+  }
+}
